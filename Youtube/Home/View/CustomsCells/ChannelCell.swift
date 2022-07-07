0f34534eae0454dd4decdfc9e536295821b28714
Marco Alonso <marco@garagecoders.net>
2022-07-07 15:12:45 -0500

    Kingfisher para descargar imagenes agregada

--- Youtube/Home/View/CustomsCells/ChannelCell.swift
+++ Youtube/Home/View/CustomsCells/ChannelCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ChannelCell: UITableViewCell {
     @IBOutlet weak var bannerImage: UIImageView!
@@ -28,6 +29,24 @@ class ChannelCell: UITableViewCell {
     func configCell(model: ChannelModel.Items){
         channelInfoLabel.text = model.snippet.description
         suscribersNumbers.text = "\(model.statistics?.subscriberCount ?? "0") suscriptores. \(model.statistics?.videoCount ?? "0") videos."
+        
+        //Banner
+        if let bannerUrl = model.brandingSettings?.image.bannerExternalUrl, let url = URL(string: bannerUrl) {
+            bannerImage.kf.setImage(with: url)
+        }
+        
+        //Profile Image
+        let imageUrl = model.snippet.thumbnails.medium.url
+        guard let url = URL(string: imageUrl) else {
+            return
+        }
+        profileImage.kf.setImage(with: url)
+        
     }
+    
+    
+
+    
+    
     
 }
