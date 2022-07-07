66b7959f71f59262d343217e70f58a17edd1aed5
Marco Alonso <marco@garagecoders.net>
2022-07-06 20:45:00 -0500

    17 CeldaChannel del Home terminada

--- Youtube/Home/View/CustomsCells/ChannelCell.swift
+++ Youtube/Home/View/CustomsCells/ChannelCell.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 class ChannelCell: UITableViewCell {
-
+    @IBOutlet weak var bannerImage: UIImageView!
+    @IBOutlet weak var channelTitle: UILabel!
+    @IBOutlet weak var suscribeLabel: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var suscribersNumbers: UILabel!
+    @IBOutlet weak var channelInfoLabel: UILabel!
+    @IBOutlet weak var bellImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        bellImage.image = UIImage(named: "bell")?.withRenderingMode(.alwaysTemplate)
+        bellImage.tintColor = UIColor(named: "grayColor")
+        
+        profileImage.layer.cornerRadius = 51/2
     }
 
     func configCell(model: ChannelModel.Items){
-        
+        channelInfoLabel.text = model.snippet.description
+        suscribersNumbers.text = "\(model.statistics?.subscriberCount ?? "0") suscriptores. \(model.statistics?.videoCount ?? "0") videos."
     }
     
 }
